26e8d912db7c55df6de5f218497bdf2bb096e445
Harsh Pancholi <harshpancholi8@gmail.com>
2019-08-13 23:38:18 +0530

    Added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
   <div id="app">
     <AlertBar />
-    <nav>
-      <div class="nav-bar">
-        <div :class="{'selected-nav' : path === '/home'}">
-          <router-link class="link" to="/home">Home</router-link>
-        </div>
-        <div :class="{'selected-nav' : path === '/upload'}">
-          <router-link tag="a" class="link" to="/upload">Upload</router-link>
-        </div>
-      </div>
-    </nav>
+    <NavBar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import AlertBar from "./components/Alert";
+import NavBar from "./components/NavBar";
 export default {
   name: "app",
-  data: () => {
-    return {
-      path: "/home"
-    };
-  },
   components: {
-    AlertBar
-  },
-  watch: {
-    $route(to, from) {
-      // update path everytime the route is changed
-      this.path = to.path;
-    }
-  },
-  mounted() {
-    this.path = this.$route.path;
-    console.log("API : ", process.env.VUE_APP_ROOT_URL);
+    AlertBar,
+    NavBar
   }
 };
 </script>
@@ -54,27 +32,6 @@ export default {
   background-color: beige;
 }
 
-.nav-bar {
-  background-color: beige;
-  height: 75px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.nav-bar div {
-  width: 50%;
-  text-align: center;
-  /* background-color: darkgray; */
-  height: 100%;
-  font-size: 25px;
-  color: white !important;
-}
-
-.selected-nav {
-  background-color: #948473 !important;
-}
-
 .none {
   display: none;
 }
@@ -82,20 +39,5 @@ export default {
 .button {
   color: white;
   background-color: burlywood;
-}
-
-li,
-li:visited,
-li:active {
-  color: burlywood;
-}
-
-.link {
-  color: burlywood;
-  display: inline-block;
-  width: 100%;
-  height: 100%;
-  cursor: pointer;
-  padding-top: 15px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav>
+    <div class="nav-bar">
+      <div :class="{'selected-nav' : path === '/home'}">
+        <router-link class="link" to="/home">Home</router-link>
+      </div>
+      <div :class="{'selected-nav' : path === '/upload'}">
+        <router-link tag="a" class="link" to="/upload">Upload</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data: () => {
+    return {
+      path: "/home"
+    };
+  },
+  watch: {
+    $route(to, from) {
+      // update path everytime the route is changed
+      this.path = to.path;
+    }
+  },
+  mounted() {
+    this.path = this.$route.path;
+    console.log("API : ", process.env.VUE_APP_ROOT_URL);
+  }
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  background-color: beige;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nav-bar div {
+  width: 50%;
+  text-align: center;
+  /* background-color: darkgray; */
+  height: 100%;
+  font-size: 25px;
+  color: white !important;
+}
+
+.selected-nav {
+  background-color: #948473 !important;
+}
+
+li,
+li:visited,
+li:active {
+  color: burlywood;
+}
+
+.link {
+  color: burlywood;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  padding-top: 15px;
+}
+</style>
