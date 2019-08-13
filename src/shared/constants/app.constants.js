ad68889fddeb0ba547c3745ec47a0daf8785999f
Harsh Pancholi <harshpancholi8@gmail.com>
2019-08-13 23:41:49 +0530

    Added max file size to env

--- src/shared/constants/app.constants.js
+++ src/shared/constants/app.constants.js
@@ -10,5 +10,5 @@ const BASE_URL = process.env.VUE_APP_ROOT_URL;
 
 const ALERT_EVENT = "ALERT_EVENT";
 
-const MAX_FILE_SIZE = 20000000;
+const MAX_FILE_SIZE = process.env.VUE_APP_MAX_FILE_SIZE;
 export { columns, BASE_URL, ALERT_EVENT, MAX_FILE_SIZE };
